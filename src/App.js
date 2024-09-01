9755a40c09c35f428403c760987b607507f1c73a
nikhipachange <nikhil1pachange@gmail.com>
2024-09-01 13:56:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import './App.css';
 function App() {
   return (
    <>
-   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark w-100">
   <a class="navbar-brand" href='/'>textutils</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+  <div class="collapse navbar-collapse "    id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto w-100">
       <li class="nav-item active">
         <a class="nav-link" href='/'>Home <span class="sr-only">(current)</span></a>
       </li>
@@ -33,7 +33,7 @@ function App() {
         <a class="nav-link disabled">Disabled</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
+    <form class="form-inline my-2 my-lg-0 bg-light" style={{width:"600px",display:'flex',gap:10,marginRight:'20px'}}>
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
